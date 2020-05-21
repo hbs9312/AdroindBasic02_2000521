b1b5f00657d68069a16f18dda4328fd5dee2154f
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 12:36:37 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/adroindbasic02_2000521/BaseActivity.java
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.adroindbasic02_2000521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+
+}
---
+++ app/src/main/java/kr/co/tjoeun/adroindbasic02_2000521/MainActivity.java
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.adroindbasic02_2000521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
