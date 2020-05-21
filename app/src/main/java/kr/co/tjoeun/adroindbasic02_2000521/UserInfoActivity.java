1bdf3065f31a198b2567df3df303069def4fac6a
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 12:44:30 +0900

    사용자 정보화면 초안 작업

--- app/src/main/java/kr/co/tjoeun/adroindbasic02_2000521/UserInfoActivity.java
+++ app/src/main/java/kr/co/tjoeun/adroindbasic02_2000521/UserInfoActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends BaseActivity {
+public class UserInfoActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_info);
         setupEvents();
         setValues();
     }
