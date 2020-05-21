5908435f559f77a62d8f1d794490cd0032e1cb22
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 12:49:50 +0900

    데이터 바인딩 세팅

--- app/src/main/java/kr/co/tjoeun/adroindbasic02_2000521/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/adroindbasic02_2000521/MainActivity.java
@@ -1,16 +1,22 @@
 package kr.co.tjoeun.adroindbasic02_2000521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 
+import kr.co.tjoeun.adroindbasic02_2000521.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setupEvents();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+ㄷ        setupEvents();
         setValues();
     }
 
